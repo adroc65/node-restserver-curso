a24ca0442512a0d5676d32e753df8e55f2b23667
Adrian Omodeo <aomodeo@gmail.com>
2019-03-03 23:29:15 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://adroc65:adroc0413O.65@cluster0-upuoe.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
