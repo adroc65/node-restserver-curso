798a8bacb1905cd8746fc5ae6638ea5a5c55bc3c
Adrian Omodeo <aomodeo@gmail.com>
2019-03-03 22:43:11 -0600

    RestServer - usuario -

--- server/config/config.js
+++ server/config/config.js
@@ -17,11 +17,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if ( process.env.NODE_ENV === 'dev' ){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
+if ( process.env.NODE_ENV === 'dev' ){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
     urlDB = 'mongodb+srv://adroc65:adroc0413O.65@cluster0-upuoe.mongodb.net/cafe'
-//}
+}
 
 process.env.URLDB = urlDB;
 
