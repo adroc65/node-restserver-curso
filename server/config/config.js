c0ba3c0037eaef8eac969b2e1c2bed81ce59c40b
Adrian Omodeo <aomodeo@gmail.com>
2019-03-06 22:54:55 -0600

    Login de Google

--- server/config/config.js
+++ server/config/config.js
@@ -36,3 +36,7 @@ if ( process.env.NODE_ENV === 'dev' ){
 
 process.env.URLDB = urlDB;
 
+// ============================
+//  Google Client ID
+// ============================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '251503079271-es54154qsi027dv4acfb2ln4l4gudba2.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
+
+// Habilitar la carpeta public, ojo el uso del "path"
+app.use(express.static( path.resolve( __dirname, '../public' )));
 
 // En /routes/index estan las rutas.
 app.use( require('./routes/index'));
